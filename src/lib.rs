752ce62de66d46fa8adcefbbc49e816f25b5aa7b
Lance Hart <shipsimfan@hotmail.com>
2024-01-22 19:57:33 -0500

    Added doc lints

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,7 @@
+//! A single-threaded async executor
 
+#![deny(missing_docs)]
+#![deny(rustdoc::private_intra_doc_links)]
+#![deny(rustdoc::unescaped_backticks)]
+#![deny(rustdoc::redundant_explicit_links)]
+#![warn(rustdoc::broken_intra_doc_links)]
