49dac9c4a5c467aa497ae865078ffc247acc1a9f
Lance Hart <shipsimfan@hotmail.com>
2024-02-06 15:06:10 -0500

    Added Windows crates

---
+++ executor/windows/src/lib.rs
@@ -0,0 +1,7 @@
+//! Executor implementation for Windows
+
+#![deny(missing_docs)]
+#![deny(rustdoc::private_intra_doc_links)]
+#![deny(rustdoc::unescaped_backticks)]
+#![deny(rustdoc::redundant_explicit_links)]
+#![warn(rustdoc::broken_intra_doc_links)]
