d5c139e3a65588e1c211c7ad4d679fa99b2b0fac
Lance Hart <shipsimfan@hotmail.com>
2024-03-01 14:58:32 -0500

    Added TimerSleep for Linux

--- futures/linux/src/time/timer/mod.rs
+++ futures/linux/src/time/timer/mod.rs
@@ -1,4 +1,9 @@
 use executor::Result;
+use std::time::Duration;
+
+mod sleep;
+
+pub use sleep::TimerSleep;
 
 /// A timer which can be used to make repeated time-based calls
 pub struct Timer {
@@ -10,5 +15,10 @@ impl Timer {
     /// Creates a new [`Timer`]
     pub fn new() -> Result<Self> {
         Ok(Timer { _priv: () })
+    }
+
+    /// Creates a [`TimerSleep`] future which yields after `duration`
+    pub fn sleep(&mut self, duration: Duration) -> Result<TimerSleep> {
+        TimerSleep::new(duration)
     }
 }
---
+++ futures/linux/src/time/timer/sleep.rs
@@ -0,0 +1,36 @@
+use crate::time::Sleep;
+use executor::Result;
+use std::{
+    future::Future,
+    marker::PhantomData,
+    pin::Pin,
+    task::{Context, Poll},
+    time::Duration,
+};
+
+/// A [`Future`] which yields after a period of time
+pub struct TimerSleep<'a> {
+    /// The actual sleep future
+    inner: Sleep,
+
+    /// A marker for the lifetime
+    _lifetime: PhantomData<&'a mut ()>,
+}
+
+impl<'a> TimerSleep<'a> {
+    /// Creates a new [`TimerSleep`] future
+    pub(super) fn new(duration: Duration) -> Result<Self> {
+        Ok(TimerSleep {
+            inner: Sleep::new(duration)?,
+            _lifetime: PhantomData,
+        })
+    }
+}
+
+impl<'a> Future for TimerSleep<'a> {
+    type Output = ();
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        Pin::new(&mut self.inner).poll(cx)
+    }
+}
