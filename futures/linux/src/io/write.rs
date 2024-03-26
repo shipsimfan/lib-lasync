df23642eb40aea36ea9bccabf206ba716e7466b4
Lance Hart <shipsimfan@hotmail.com>
2024-03-26 14:05:38 -0400

    Added write_all

--- futures/linux/src/io/write.rs
+++ futures/linux/src/io/write.rs
@@ -5,4 +5,19 @@ use std::future::Future;
 pub trait Write {
     /// Attempts to write the contents of `buf` into this, returning the number of bytes written
     fn write<'a>(&'a mut self, buf: &'a [u8]) -> impl Future<Output = Result<usize>> + 'a;
+
+    /// Attempts to write all the contents of `buf` in this, returning an error if it is unable to
+    fn write_all<'a>(&'a mut self, mut buf: &'a [u8]) -> impl Future<Output = Result<()>> + 'a {
+        async move {
+            while !buf.is_empty() {
+                match self.write(buf).await {
+                    Ok(0) => return Err(linux::Error::new(linux::errno::ECONNRESET)),
+                    Ok(n) => buf = &buf[n..],
+                    Err(error) => return Err(error),
+                }
+            }
+
+            Ok(())
+        }
+    }
 }
