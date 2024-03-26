9f5d1696ef353d085951d443912e5e1298699a29
Lance Hart <shipsimfan@hotmail.com>
2024-03-26 14:03:52 -0400

    Added read_exact

--- futures/linux/src/io/read.rs
+++ futures/linux/src/io/read.rs
@@ -6,4 +6,27 @@ use executor::Result;
 pub trait Read {
     /// Attempts to read from this into `buf`, returning the number of bytes read
     fn read<'a>(&'a mut self, buf: &'a mut [u8]) -> impl Future<Output = Result<usize>> + 'a;
+
+    /// Attempts to read from this and fill `buf`, returning an error if it is unable to
+    fn read_exact<'a>(
+        &'a mut self,
+        mut buf: &'a mut [u8],
+    ) -> impl Future<Output = Result<()>> + 'a {
+        async move {
+            while !buf.is_empty() {
+                match self.read(buf).await {
+                    Ok(0) => break,
+                    Ok(n) => buf = &mut buf[n..],
+                    Err(error) => return Err(error),
+                }
+            }
+
+            if buf.is_empty() {
+                Ok(())
+            } else {
+                // TODO: Find a better error for this, or create a custom one
+                Err(linux::Error::new(linux::errno::ECONNRESET))
+            }
+        }
+    }
 }
