7805da892483ef5c0432388f09df58c68d66cf1a
Lance Hart <shipsimfan@hotmail.com>
2024-04-12 14:11:25 -0400

    Added file reading and writing

--- futures/linux/src/fs/file.rs
+++ futures/linux/src/fs/file.rs
@@ -1,6 +1,11 @@
 use linux::unistd::close;
 use std::ffi::c_int;
 
+use crate::{
+    fd::{AsFD, FDRead, FDWrite},
+    io::{Read, Write},
+};
+
 /// An open file on the filesystem
 pub struct File(c_int);
 
@@ -8,6 +13,30 @@ impl File {
     /// Creates a new [`File`] for `fd`
     pub(super) fn new(fd: c_int) -> Self {
         File(fd)
+    }
+}
+
+impl AsFD for File {
+    unsafe fn fd(&self) -> c_int {
+        self.0
+    }
+}
+
+impl Read for File {
+    fn read<'a>(
+        &'a mut self,
+        buf: &'a mut [u8],
+    ) -> impl std::future::Future<Output = linux::Result<usize>> + 'a {
+        FDRead::new(self, buf)
+    }
+}
+
+impl Write for File {
+    fn write<'a>(
+        &'a mut self,
+        buf: &'a [u8],
+    ) -> impl std::future::Future<Output = linux::Result<usize>> + 'a {
+        FDWrite::new(self, buf)
     }
 }
 
--- tests/file.rs
+++ tests/file.rs
@@ -1,15 +1,22 @@
-use lasync::fs::OpenOptions;
+use lasync::{fs::OpenOptions, io::Read};
 use std::num::NonZeroUsize;
 
 const SIZE: NonZeroUsize = unsafe { NonZeroUsize::new_unchecked(32) };
 const READ_PATH: &str = "./tests/test_data.txt";
 
+const TEST_CONTENT: &[u8] = include_bytes!("./test_data.txt");
+
 #[test]
 fn file_read() {
     lasync::run(SIZE, async {
-        let file = OpenOptions::new().read(true).open(READ_PATH).await.unwrap();
+        let mut file = OpenOptions::new().read(true).open(READ_PATH).await.unwrap();
 
-        drop(file);
+        let mut buffer = [0; TEST_CONTENT.len()];
+        file.read_exact(&mut buffer).await.unwrap();
+
+        assert_eq!(buffer, TEST_CONTENT);
+
+        println!("{}", String::from_utf8_lossy(&buffer));
     })
     .unwrap();
 }
